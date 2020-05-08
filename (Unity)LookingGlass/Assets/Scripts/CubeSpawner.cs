8cfd1bde7c2188297f2ed20f73f893e4c3d696d4
ColinvD <22950@ma-web.nl>
2020-05-08 16:06:20 +0200

    start object interactions

---
+++ (Unity)LookingGlass/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject cube;
+    [SerializeField] private float spawnSpeed;
+    [SerializeField] private float destroySpeed;
+
+    private float timer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnSpeed)
+        {
+            timer = 0;
+            SpawnCube();
+        }
+    }
+
+    private void SpawnCube()
+    {
+        GameObject newCube = Instantiate(cube, transform.position, transform.rotation);
+        Destroy(newCube, destroySpeed);
+    }
+}
