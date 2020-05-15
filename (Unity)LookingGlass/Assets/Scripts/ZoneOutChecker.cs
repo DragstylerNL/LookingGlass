1c84214c819c8c2d71c22353d37d597994f132c4
ColinvD <22950@ma-web.nl>
2020-05-15 14:46:11 +0200

    added 2 hats and made them respawn if out of reach

---
+++ (Unity)LookingGlass/Assets/Scripts/ZoneOutChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneOutChecker : MonoBehaviour
+{
+    [SerializeField] private Transform transform;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("HandZone"))
+        {
+            Invoke("Reset", 2f);
+        }
+    }
+
+    private void Reset()
+    {
+        gameObject.transform.position = transform.position;
+        gameObject.transform.rotation = transform.rotation;
+    }
+}
