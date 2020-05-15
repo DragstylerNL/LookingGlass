5e3cb2310618e69d554588331afcb2d16938b0c4
ColinvD <22950@ma-web.nl>
2020-05-15 11:46:59 +0200

    added interaction engine from leap and made the cubes able to pick up

--- (Unity)LookingGlass/Assets/Scripts/CubeSpawner.cs
+++ (Unity)LookingGlass/Assets/Scripts/CubeSpawner.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Leap.Unity.Interaction;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CubeSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject cube;
     [SerializeField] private float spawnSpeed;
     [SerializeField] private float destroySpeed;
+    [SerializeField] private InteractionManager manager;
 
     private float timer = 0f;
 
@@ -25,6 +28,7 @@ public class CubeSpawner : MonoBehaviour
     private void SpawnCube()
     {
         GameObject newCube = Instantiate(cube, transform.position, transform.rotation);
+        newCube.GetComponent<InteractionBehaviour>().manager = manager;
         Destroy(newCube, destroySpeed);
     }
 }
