155d94276b8a99e22cc40d3373a8606b4249f8f6
DragstylerNL <dragstylernl@gmail.com>
2020-06-26 14:07:38 +0200

    socket io and connection between unity and server

---
+++ (Unity)LookingGlass/Assets/_Scripts/NetworkClient.cs
@@ -0,0 +1,48 @@
+﻿using SocketIO;
+using UnityEngine;
+
+public class NetworkClient : SocketIOComponent
+{
+    // __________________________________________________________________________________________________________/ Start
+    public override void Start()
+    {
+        base.Start();
+        
+        SetupEvents();
+    }
+    
+    // __________________________________________________________________________________________________/ Set up Events
+    private void SetupEvents()
+    {
+        On("open", (data) =>
+        {
+            print("connection made ^^");
+        });
+    }
+
+    public bool fuck = false;
+    void Update()
+    {
+        if (fuck)
+        {
+            fuck = false;
+            SendUpdate("zoo");
+        }
+    }
+    
+    // ____________________________________________________________________________________________________/ Update Area 
+    public void SendUpdate(string area)
+    {
+        Emit("areaUpdate", new JSONObject(JsonUtility.ToJson(new JsonArea(area))));
+    }
+
+}
+
+public class JsonArea
+{
+    public string area;
+    public JsonArea(string area)
+    {
+        this.area = area;
+    }
+}
