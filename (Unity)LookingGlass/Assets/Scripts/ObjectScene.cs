bde59fb82300243e7f2d3920afc3784a14e972b4
ColinvD <22950@ma-web.nl>
2020-06-26 15:56:15 +0200

    interactions changed to book(s)

--- (Unity)LookingGlass/Assets/Scripts/ObjectScene.cs
+++ (Unity)LookingGlass/Assets/Scripts/ObjectScene.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Leap.Unity.Interaction;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class ObjectScene : MonoBehaviour
 {
@@ -10,13 +7,14 @@ public class ObjectScene : MonoBehaviour
     [SerializeField] private GameObject floor;
     [SerializeField] private Transform wantedObjectPos;
     [SerializeField] private float closeEnoughDistance = 0.125f;
-    public string name;
     private bool _onit = false;
     private bool grabbed = false;
     private Rigidbody _rigidbody;
     [SerializeField] private float maxWaitTime;
     private float currentTime = 0f;
     private bool allowed = true;
+    public UnityEvent _sceneActive;
+    public UnityEvent _sceneDeactive;
 
     void Start()
     {
@@ -31,7 +29,6 @@ public class ObjectScene : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Test");
             transform.position = wantedObjectPos.position;
             transform.rotation = wantedObjectPos.rotation;
         }
@@ -41,51 +38,28 @@ public class ObjectScene : MonoBehaviour
         {
             floor.GetComponent<MeshRenderer>().material.color = Color.white;
             _rigidbody.constraints = RigidbodyConstraints.None;
+            _sceneDeactive.Invoke();
             _onit = false;
         }
-        /*RaycastHit hit;
-        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 10f))
-        {
-            floor.GetComponent<MeshRenderer>().material.color = Color.white;
-            _rigidbody.constraints = RigidbodyConstraints.None;
-            _onit = false;
-        }
-        else
-        {
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 10f))
-            {
-                if (!hit.collider.CompareTag("Character"))
-                {
-                    floor.GetComponent<MeshRenderer>().material.color = Color.white;
-                    _rigidbody.constraints = RigidbodyConstraints.None;
-                    _onit = false;
-                }
-            }
-        }*/
     }
 
     public void SetWorld()
     {
-        //RaycastHit hit;
-        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 10f))
-        //{
-            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.blue);
-            if (CloseEnough())
-            {
-                grabbed = false;
-                floor.GetComponent<MeshRenderer>().material.color = color;
-                _onit = true;
-                _rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-                allowed = false;
-                currentTime = 0;
-            }
-        //}
+        if (CloseEnough())
+        {
+            grabbed = false;
+            floor.GetComponent<MeshRenderer>().material.color = color;
+            _sceneActive.Invoke();
+            _onit = true;
+            _rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            allowed = false;
+            currentTime = 0;
+        }
     }
 
     private bool CloseEnough()
     {
         float distance = Vector3.Distance(this.transform.position, wantedObjectPos.position);
-        Debug.Log(distance);
         
         if (distance < closeEnoughDistance)
         {
